f8a13fc2b732678ec042bfbdf53656523da8b5de
DayaRC26 <d.rios1@utp.edu.co>
2024-03-11 23:40:34 -0500

    Informacion sobre el DataFrame

--- Proyecto API/API/api.py
+++ Proyecto API/API/api.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sodapy import Socrata
 
 #obtiene  la informacion del API, recibe dos parametros
@@ -11,8 +12,27 @@ def api_casos(limit_records, departament):
     results_df = pd.DataFrame(results)
 
     if 'pais_viajo_1_nom' not in results_df.columns:
-        results_df['pais_viajo_1_nom'] = "Sin Registro"
+        results_df['pais_viajo_1_nom'] = pd.NA
+
+    results_df.fillna("Sin Registro", inplace = True)
+    data_information (results_df)
+
     
     return results_df
+
+def data_information (dataf):
+    print("\nInformacion general:")
+    print("Numero de filas:",dataf.shape[0])
+    print("Numero de columnas:",dataf.shape[1] )
+    print("Nombre Columnas:",dataf.columns.values.tolist())
+    print("Tipo de datos columnas:\n",dataf.dtypes)
+    print("\nInformacion valores perdidos:")
+    print("Columna con valores perdidos:",dataf.columns[dataf.isnull().any()].tolist())
+    print("Numero de Filas con valores perdidos:", len(dataf[pd.isnull(dataf).any(axis=1)]))
+    print("Indices con valores perdidos: ", np.where(pd.isnull(dataf).any())[0][:5])
+    print("\nEstadísticas Generales:")
+    print(dataf.info())
+    print("\nResumen de estadisticas:" )
+    print(dataf.describe())
 
 
